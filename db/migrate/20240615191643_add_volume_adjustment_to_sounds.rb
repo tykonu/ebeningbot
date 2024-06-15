e723d842b42b3ce6f8dc6becc46e7d063453322d
rasmus <tykonu@gmail.com>
2024-06-15 22:40:00 +0300

    Add volume adjustment features

--- db/migrate/20240615191643_add_volume_adjustment_to_sounds.rb
+++ db/migrate/20240615191643_add_volume_adjustment_to_sounds.rb
@@ -1,4 +1,4 @@
-class AddVolumeAdjustmentToSounds < ActiveRecord::Migration[7.1]
+class AddVolumeAdjustmentToSounds < ActiveRecord::Migration[7.0]
   def change
     add_column :sounds, :volume_adjustment, :decimal, precision: 5, scale: 2
   end
